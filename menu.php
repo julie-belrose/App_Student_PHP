2eed90ac0b75ebc5d35146a1fd4b08e0da7e1273
Julie-ANANI <65860846+Julie-ANANI@users.noreply.github.com>
2025-06-18 12:10:11 +0200

    refacto part CLI

---
+++ menu.php
@@ -0,0 +1,20 @@
+<?php
+function displayMenu(): void
+{
+    echo "
+       _             _ _             _
+   ___| |_ _   _  __| (_) __ _ _ __ | |_ ___
+  / _ \ __| | | |/ _` | |/ _` | '_ \| __/ __|
+ |  __/ |_| |_| | (_| | | (_| | | | | |_\__ \\
+  \___|\__|\__,_|\__,_|_|\__,_|_| |_|\__|___/
+
+1. Display students
+2. Create a student
+3. Edit a student
+4. Delete a student
+5. Search by name or surname
+6. Exit
+7. Display last 10 logs
+8. Clear logs
+";
+}
